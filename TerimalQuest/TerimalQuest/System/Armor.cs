7765c248459580ca47e6234ceb70ee7219c903cb
UihwanLee <luh5063014@naver.com>
2025-10-14 16:50:49 +0900

    Feat: Item을 상속하는 Armor, Weapon 클래스 생성

---
+++ TerimalQuest/TerimalQuest/System/Armor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TerimalQuest.Core;
+
+namespace TerimalQuest.System
+{
+    public class Armor : Item
+    {
+        public float def;
+
+        public Armor(int id, string name, string desc, int price, float def, ItemType type) : base(id, name, desc, price, type)
+        {
+            this.def = def;
+        }
+
+        public override void Equip()
+        {
+            base.Equip();
+
+            // 플레이어 방어구 장착
+            
+        }
+
+        public override void DisplayInfo()
+        {
+            // 아이템 정보 표시
+            //Console.WriteLine($"{name}")
+        }
+    }
+}
---
+++ TerimalQuest/TerimalQuest/System/Weapon.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TerimalQuest.System
+{
+    public class Weapon : Item
+    {
+        public float atk;
+
+        public Weapon(int id, string name, string desc, int price, float atk, ItemType type) : base(id, name, desc, price, type)
+        {
+            this.atk = atk;
+        }
+
+        public override void Equip()
+        {
+            base.Equip();
+
+            // 플레이어 방어구 장착
+
+        }
+    }
+}
