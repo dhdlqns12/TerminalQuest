b5dab3451447afdd1fc948b42e72763b195049d9
UihwanLee <luh5063014@naver.com>
2025-10-16 14:50:16 +0900

    Refactor: GetUserChoice 공통으로 쓰는 부분 ConsoleHelper 클래스로 이동

--- TerimalQuest/TerimalQuest/Scenes/ShopSaleScene.cs
+++ TerimalQuest/TerimalQuest/Scenes/ShopSaleScene.cs
@@ -39,7 +39,7 @@ namespace TerimalQuest.Scenes
             // 인벤토리 인덱스 정보 가져오기
             int vaildCount = player.inventory.Items.Count;
             string[] vaildItemOption = Enumerable.Range(0, vaildCount + 1).Select(i => i.ToString()).ToArray();   // LINQ 문법
-            var choice = GetUserChoice(vaildItemOption);
+            var choice = ConsoleHelper.GetUserChoice(vaildItemOption);
 
             // 아이템 판매
             while (true)
@@ -49,28 +49,12 @@ namespace TerimalQuest.Scenes
                 // 판매 가능한 아이템이면 판매 
                 if (shop.TrySaleItem(int.Parse(choice) - 1) == true) break;
 
-                choice = GetUserChoice(vaildItemOption);
+                choice = ConsoleHelper.GetUserChoice(vaildItemOption);
             }
 
             // 판매가 완료 되었으면 1초 후 갱신
             Thread.Sleep(1000);
             OnSceneChangeRequested?.Invoke(new ShopSaleScene());
-        }
-
-        // 사용자 입력 체크
-        protected string GetUserChoice(string[] vaildOptions)
-        {
-            string choice;
-            while (true)
-            {
-                Console.Write(">> ");
-                choice = Console.ReadLine();
-                Console.WriteLine();
-
-                foreach (var option in vaildOptions) if (choice == option) return choice;
-
-                UIManager.Instance.SelectWrongSelection();
-            }
         }
     }
 }
